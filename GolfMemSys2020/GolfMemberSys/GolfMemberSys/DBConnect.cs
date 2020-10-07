be74ba5a921fb2fa28683223e5fc76619633cf0a
Basic Java Coder <43179251+basicJavaCoder@users.noreply.github.com>
2020-10-07 16:44:37 +0100

    Update DBConnect.cs

--- GolfMemSys2020/GolfMemberSys/GolfMemberSys/DBConnect.cs
+++ GolfMemSys2020/GolfMemberSys/GolfMemberSys/DBConnect.cs
@@ -5,9 +5,9 @@ namespace GolfMemberSys
     class DBConnect
     {
         //use this connection string when using your laptop
-        public const String oradb = "Data Source = localhost/orcl; User Id = system; Password = ;";
+        public const String oradb = "Data Source = localhost/orcl; User Id = ; Password = ;";
 
-        //use this connection string in ITT
+        //use this connection string in college
         //public const String oradb = "Data Source = oracle/orcl; User Id = ; Password = ;";
     }
 }
